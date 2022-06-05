2c7dfdc0e8a81dac46cd34587e7b63b91c58b86b
VilonovLev <vilonovlev@gmail.com>
2022-06-06 00:14:36 +0300

    add result

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numFirst = 5;
 int numSecond = 3;
 int result = numFirst + numSecond;
-Console.WriteLine(numFirst + " + " + numSecond + " = " + r);
+Console.WriteLine(numFirst + " + " + numSecond + " = " + result);
 
