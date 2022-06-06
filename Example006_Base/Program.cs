1384735a1551918f3b9d2f7f55ebc5e7c6fdf29a
VilonovLev <vilonovlev@gmail.com>
2022-06-06 15:45:23 +0300

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 10;
+int b = 13;
+int c = 15;
+int d = 5;
+int f = 8;
+int result = a;
+int max = Math.Max(a,Math.Max(Math.Max(b,c),Math.Max(d,f)));
+
+if (b > result) result = b;
+if (c > result) result = c;
+if (d > result) result = d;
+if (f > result) result = f;
+
+Console.WriteLine("max = " + result);
+Console.WriteLine("maxF = " + max);
